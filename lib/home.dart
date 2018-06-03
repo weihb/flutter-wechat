b11956760efd4445458f54af567159cd3fefdd87
herbie <weihongbao0901@163.com>
2018-06-03 12:47:07 +0800

    UI修改

--- lib/home.dart
+++ lib/home.dart
@@ -11,164 +11,180 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('基本控件使用'),
-        titleSpacing: 50.0, //标题间距
-        centerTitle: true,
-        actions: <Widget>[
-          new GestureDetector(
-              child: new Container(
-                child: new Icon(
-                  Icons.map,
-                  color: Colors.green,
-                  semanticLabel: '地图',
-                  textDirection: TextDirection.rtl,
-                ),
-                padding: const EdgeInsets.all(10.0),
-              ),
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return new AlertDialog(
-                        title: new Text('点击事件'),
-                        content: new Text('点击了地图按钮'),
-                      );
-                    });
-              }),
-          new Icon(
-            Icons.menu,
-            color: Colors.black,
-          ),
-          new Icon(
-            Icons.more,
-            color: Colors.yellow,
-          )
-        ],
-        leading: new CircleAvatar(
-          backgroundColor: Colors.pink[200],
-          child: new Text('head'),
-          foregroundColor: Colors.blue,
+        appBar: new AppBar(
+          title: new Text('Flutter学习大纲'),
+          titleSpacing: 50.0, //标题间距
+          centerTitle: true,
+          //   actions: <Widget>[
+          //     new GestureDetector(
+          //         child: new Container(
+          //           child: new Icon(
+          //             Icons.map,
+          //             color: Colors.green,
+          //             semanticLabel: '地图',
+          //             textDirection: TextDirection.rtl,
+          //           ),
+          //           padding: const EdgeInsets.all(10.0),
+          //         ),
+          //         onTap: () {
+          //           showDialog(
+          //               context: context,
+          //               builder: (BuildContext context) {
+          //                 return new AlertDialog(
+          //                   title: new Text('点击事件'),
+          //                   content: new Text('点击了地图按钮'),
+          //                 );
+          //               });
+          //         }),
+          //     new Icon(
+          //       Icons.menu,
+          //       color: Colors.black,
+          //     ),
+          //     new Icon(
+          //       Icons.more,
+          //       color: Colors.yellow,
+          //     )
+          //   ],
+          //   leading: new CircleAvatar(
+          //     backgroundColor: Colors.pink[200],
+          //     child: new Text('head'),
+          //     foregroundColor: Colors.blue,
+          //   ),
+          //   flexibleSpace: new Text('flexibleSpace'),
+          //   bottom: new PreferredSize(
+          //       child: new Text('PreferredSize'),
+          //       preferredSize: new Size(20.0, 20.0)),
         ),
-        flexibleSpace: new Text('flexibleSpace'),
-        bottom: new PreferredSize(
-            child: new Text('PreferredSize'),
-            preferredSize: new Size(20.0, 20.0)),
-      ),
-      bottomNavigationBar: new BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-            print('index: $index');
-          },
-          items: [
-            new BottomNavigationBarItem(
-                title: new Text('首页'),
-                backgroundColor: Colors.blue,
-                icon: new Icon(
-                  Icons.home,
-                )),
-            new BottomNavigationBarItem(
-                title: new Text('发现'),
-                backgroundColor: Colors.blue,
-                icon: new Icon(
-                  Icons.find_in_page,
-                )),
-            new BottomNavigationBarItem(
-                title: new Text('我的'),
-                backgroundColor: Colors.blue,
-                icon: new Icon(
-                  Icons.message,
-                )),
-          ]),
-      body: new Container(
-          child: new ListView(
+        bottomNavigationBar: new BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+              print('index: $index');
+            },
+            items: [
+              new BottomNavigationBarItem(
+                  title: new Text('首页'),
+                  backgroundColor: Colors.blue,
+                  icon: new Icon(
+                    Icons.home,
+                  )),
+              new BottomNavigationBarItem(
+                  title: new Text('发现'),
+                  backgroundColor: Colors.blue,
+                  icon: new Icon(
+                    Icons.find_in_page,
+                  )),
+              new BottomNavigationBarItem(
+                  title: new Text('我的'),
+                  backgroundColor: Colors.blue,
+                  icon: new Icon(
+                    Icons.message,
+                  )),
+            ]),
+        body: new GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(10.0),
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+          crossAxisCount: 2,
+          children: <Widget>[
+            new ItemWidget(
+              title: '基础组件',
+              describe: '在构建您的第一个Flutter应用程序之前，您绝对需要了解这些widget。',
+            ),
+            new ItemWidget(
+              title: 'Material Components',
+              describe: '实现了Material Design 指南的视觉、效果、motion-rich的widget。',
+            ),
+            new ItemWidget(
+              title: 'Cupertino(iOS风格的widget)',
+              describe: '用于当前iOS设计语言的美丽和高保真widget。',
+            ),
+            new ItemWidget(
+              title: 'Layout',
+              describe: '排列其它widget的columns、rows、grids和其它的layouts。',
+            ),
+            new ItemWidget(
+              title: 'Text',
+              describe: '文本显示和样式',
+            ),
+            new ItemWidget(
+              title: 'Assets、图片、Icons',
+              describe: '管理assets, 显示图片和Icon。',
+            ),
+            new ItemWidget(
+              title: ' Input',
+              describe: 'Material Components 和 Cupertino中获取用户输入的widget。',
+            ),
+            new ItemWidget(
+              title: '动画和Motion',
+              describe: '在您的应用中使用动画。查看Flutter中的动画总览',
+            ),
+            new ItemWidget(
+              title: '交互模型',
+              describe: '响应触摸事件并将用户路由到不同的页面视图（View）。',
+            ),
+            new ItemWidget(
+              title: '样式',
+              describe: '管理应用的主题，使应用能够响应式的适应屏幕尺寸或添加填充。',
+            ),
+             new ItemWidget(
+              title: '绘制和效果',
+              describe: 'Widget将视觉效果应用到其子组件，而不改变它们的布局、大小和位置。',
+            ),
+             new ItemWidget(
+              title: 'Async',
+              describe: 'Flutter应用的异步模型',
+            ),
+             new ItemWidget(
+              title: '样式',
+              describe: '管理应用的主题，使应用能够响应式的适应屏幕尺寸或添加填充。',
+            ),
+             new ItemWidget(
+              title: '样式',
+              describe: '管理应用的主题，使应用能够响应式的适应屏幕尺寸或添加填充。',
+            ),
+          ],
+        ));
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  final String title;
+  final String describe;
+  ItemWidget({Key key, this.title, this.describe}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      padding: const EdgeInsets.all(10.0),
+      child: new Column(
         children: <Widget>[
           new Text(
-            'TextView',
+            '$title',
             style: new TextStyle(
-                color: Colors.pink,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                fontFamily: 'fontFamily',
-                // textBaseline: TextBaseline.alphabetic,
-                // locale: new Locale('007'),
-                // decoration: TextDecoration.overline,
-                debugLabel: 'debugLabel'),
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold),
           ),
-          new Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              new Text('华丽分割线'),
-              new Divider(
-                color: Colors.black45,
-                height: 2.0,
-              ),
-            ],
+          new Text(
+            '$describe',
+            style: new TextStyle(
+                color: Colors.blue,
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold),
           ),
-          new RichText(
-              maxLines: 3,
-              text: new TextSpan(
-                text: 'RichText ',
-                style: DefaultTextStyle.of(context).style,
-                children: <TextSpan>[
-                  new TextSpan(
-                      text: 'bold',
-                      style: new TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0)),
-                  new TextSpan(text: ' world!'),
-                ],
-              )),
-          new Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              new Text('华丽分割线'),
-              new Divider(
-                color: Colors.black45,
-                height: 2.0,
-              ),
-            ],
-          ),
-          // new InputChip(label: new Text('input')),
-          new Form(
-            child: new TextField(
-              decoration: const InputDecoration(labelText: '手机号'),
-              onChanged: (text) {
-                //当正在编辑的文本改变时调用。
-                print('onChanged:$text');
-              },
-              onSubmitted: (text) {
-                //当用户指示他们已经完成编辑字段中的文本时调用。
-                print('onSubmitted:$text');
-              }, 
-            ),
-          ),
-          new TextField(
-            decoration: const InputDecoration(labelText: '密码'),
-            onChanged: (text) {
-              print('onChanged:$text');
-            },
-            onSubmitted: (text) {
-              print('onSubmitted:$text');
-            },
-          ),
-          new Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              new Text('华丽分割线'),
-              new Divider(
-                color: Colors.black45,
-                height: 2.0,
-              ),
-            ],
-          ),
-
         ],
+      ),
+      decoration: const BoxDecoration(
+          border: const Border(
+        top: const BorderSide(width: 1.0, color: const Color(0xFFFF9c9c9c)),
+        left: const BorderSide(width: 1.0, color: const Color(0xFFFF9c9c9c)),
+        right: const BorderSide(width: 1.0, color: const Color(0xFFFF9c9c9c)),
+        bottom: const BorderSide(width: 1.0, color: const Color(0xFFFF9c9c9c)),
       )),
     );
   }
---
+++ lib/widgets/BasicsWeigets.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class BasicsWeigets extends StatefulWidget {
+  @override
+  BasicState createState() => new BasicState();
+}
+
+class BasicState extends State<BasicsWeigets> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  new Container(
+          child: new ListView(
+        children: <Widget>[
+          new Text(
+            'TextView',
+            style: new TextStyle(
+                color: Colors.pink,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                fontFamily: 'fontFamily',
+                // textBaseline: TextBaseline.alphabetic,
+                // locale: new Locale('007'),
+                // decoration: TextDecoration.overline,
+                debugLabel: 'debugLabel'),
+          ),
+          new Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              new Text('华丽分割线'),
+              new Divider(
+                color: Colors.black45,
+                height: 2.0,
+              ),
+            ],
+          ),
+          new RichText(
+              maxLines: 3,
+              text: new TextSpan(
+                text: 'RichText ',
+                style: DefaultTextStyle.of(context).style,
+                children: <TextSpan>[
+                  new TextSpan(
+                      text: 'bold',
+                      style: new TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0)),
+                  new TextSpan(text: ' world!'),
+                ],
+              )),
+          new Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              new Text('华丽分割线'),
+              new Divider(
+                color: Colors.black45,
+                height: 2.0,
+              ),
+            ],
+          ),
+          // new InputChip(label: new Text('input')),
+          new Form(
+            child: new TextField(
+              textAlign: TextAlign.center,
+              decoration: const InputDecoration(labelText: '手机号'),
+              onChanged: (text) {
+                //当正在编辑的文本改变时调用。
+                print('onChanged:$text');
+              },
+              onSubmitted: (text) {
+                //当用户指示他们已经完成编辑字段中的文本时调用。
+                print('onSubmitted:$text');
+              },
+            ),
+          ),
+          new TextField(
+            decoration: const InputDecoration(
+                icon: const Icon(Icons.mode_edit), labelText: '密码'),
+            onChanged: (text) {
+              print('onChanged:$text');
+            },
+            onSubmitted: (text) {
+              print('onSubmitted:$text');
+            },
+          ),
+          new Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              new Text('华丽分割线'),
+              new Divider(
+                color: Colors.black45,
+                height: 2.0,
+              ),
+            ],
+          ),
+        ],
+      ));
+  }
+}
